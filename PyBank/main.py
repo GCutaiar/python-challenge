bb3c2500f1f22d3f5f12b2ec9bef7352d789f74b
GRC <gcutaiar@yahoo.com>
2019-08-24 15:24:52 -0500

    Completed HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,60 @@
-PyBank
+#PyBank
+import os
+import csv
+
+def retrievebankinfo(csv):
+    totalPnL = 0
+    maxdifrev = 0
+    mindifrev = 0
+    months = 0
+    avgchange = 0
+    maxdifmonth = ""
+    mindifmonth = ""
+    lastmonth = None
+    lastmonth_PnL = 0
+    changes = []
+    for row in csv:
+        current_month = row[0]
+        current_PnL = int(row[1])
+        totalPnL += current_PnL
+        months += 1
+        if lastmonth is not None:
+            current_change = current_PnL - lastmonth_PnL
+            changes.append(current_change)
+            if current_change > maxdifrev:
+                maxdifrev = current_change
+                maxdifmonth = current_month
+            if current_change < mindifrev:
+                mindifrev = current_change
+                mindifmonth = current_month
+        lastmonth = current_month
+        lastmonth_PnL = current_PnL
+    avgchange = sum(changes)/len(changes)
+    return [months,totalPnL, maxdifrev, mindifrev, avgchange, maxdifmonth, mindifmonth]
+
+with open ("budget_data.csv") as file:
+    csvreader = csv.reader(file, delimiter = ",")   
+    header = next(csvreader)
+    analysis = retrievebankinfo(csvreader)
+
+print(f"""
+Financial Analysis
+__________________________
+Total Months: {analysis[0]}
+Total: ${round(analysis[1], 2)}
+Average Change: ${round(analysis[4], 2)}
+Greatest Increase: {analysis[5]} (${analysis[2]})
+Greatest Decrease: {analysis[6]} (${analysis[3]})
+    """)
+
+
+with open ("output.txt","w") as homework:
+    homework.writelines(f"""
+Financial Analysis
+__________________________
+Total Months: {analysis[0]}
+Total: ${round(analysis[1], 2)}
+Average Change: ${round(analysis[4], 2)}
+Greatest Increase: {analysis[5]} (${analysis[2]})
+Greatest Decrease: {analysis[6]} (${analysis[3]})
+    """)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,36 @@
-PyPoll
+#PyPoll
+import os
+import csv
+
+with open ("output.txt","w") as homework:
+
+    candidates = {}
+    filepath = os.path.join("Resources", "election_data.csv")
+    with open (filepath) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ",")   
+        header = next(csvreader)
+        for row in csvreader:
+            candidate = row[2]
+            if candidate in candidates.keys():
+                candidates[candidate] += 1
+            else:
+                candidates[candidate] = 1
+    total_votes = sum(candidates.values())
+    print("Elections Results")
+    homework.writelines("Elections Results\n")
+    greatest_votes = 0
+    winner = ""
+    for key, value in candidates.items():
+        vote_percent = round((value/total_votes)*100,2)
+        print(f'Candidate: {key}, Percent of Votes: {vote_percent}%, (Votes: {value})')
+        homework.writelines(f'Candidate: {key}, Percent of Votes: {vote_percent}%, (Votes: {value})\n')
+        if value > greatest_votes:
+            greatest_votes = value
+            winner = key
+
+
+    print(f'Total Votes: {total_votes}')
+    homework.writelines(f'Total Votes: {total_votes}\n')
+    print(f'Winner: {winner}!')
+    homework.writelines(f'Winner: {winner}!\n')
+
